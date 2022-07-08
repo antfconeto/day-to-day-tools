5cdc09a0c1cf73e93440208013c9abeae60139f2
Antonio Francisco de Araújo Neto <antfcone@gmail.com>
2022-07-08 18:20:12 -0300

    settings

--- src/routes/routers.jsx
+++ src/routes/routers.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-
+import Home from "../pages/Home/Home";
 const Routers = ()=>{
     return(
         <Routes>
-            <Route path="/" element={<h1>Home</h1>}></Route>
+            <Route path="/" element={<Home/>}></Route>
             <Route path="/about" element={<h1>Home</h1>}></Route>
             <Route path="/*" element={<Navigate to="/"/>}></Route>
         </Routes>
